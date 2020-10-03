1b9c93cb239147c4c045359b6997cdcadfe16763
Ariel <norbertowinkler@gmail.com>
2020-10-03 05:17:35 -0300

    InteracPlayer01
    Junta Objetos, y teleport mal

---
+++ Pasco/Assets/Scripts/Player/PlayerInteraction.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    public List<GameObject> recolectables;
+    List<string> list = new List<string>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddObjectToList(string value)
+    {
+        list.Add((string)value);
+        foreach(GameObject go in recolectables)
+        {
+            if (!go.activeSelf)
+            {
+                go.SetActive(true);
+                break;
+            }                
+        }
+        foreach(string item in list)
+        {
+            Debug.Log(item);
+        }
+
+    }
+
+}
---
+++ Pasco/Assets/Scripts/Player/SpawnPlayer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPlayer : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject spawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        //if (other.gameObject.tag.Equals("Player"))
+        //{
+        //    player.transform.position = spawn.transform.position;
+        //}
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+        {
+            player.GetComponent<CharacterController>().Move(spawn.transform.position);
+        }
+    }
+}
