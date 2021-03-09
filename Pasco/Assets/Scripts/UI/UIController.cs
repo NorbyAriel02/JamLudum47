149a09ac340909eaffb1da862ee91f354af2a318
NorbyAriel02 <norbertowinkler@gmail.com>
2021-03-09 19:21:49 -0300

    Level 1 - Game Creator
    Molinetes con animacion
    Puerta de la boleteria
    Fin de escales y tunels
    Escaleras de mano
    Maquina de boleteria
    Notas
    Llave

--- Pasco/Assets/Scripts/UI/UIController.cs
+++ Pasco/Assets/Scripts/UI/UIController.cs
@@ -1,75 +1,36 @@
-﻿using System.Collections.Generic;
-using UnityEngine.UI;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class UIController : MonoBehaviour
 {
-    public UnityStandardAssets.Characters.FirstPerson.FirstPersonController cc;
-    public GameObject panelNotes;
-    public List<Button> goListaDeNotas;
-    public Text txtNota;
-    private bool notesAreOpen = false;    
-    private Inventory inventory;
+    // Start is called before the first frame update
     void Start()
     {
-        for (int x = 0; x < 8; x++)
-        {
-            int index = x;
-            goListaDeNotas[x].onClick.AddListener(delegate { GetNote(index); });
-        }
-        foreach (Button btn in goListaDeNotas)
-        {
-            btn.gameObject.SetActive(false);
-        }
-        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+        
     }
-    void GetNote(int index)
+
+    private void OnEnable()
     {
-        txtNota.text = inventory.GetNote(index);
-    }    
+        UIHelper.UIOpen();
+    }
+
+    private void OnDisable()
+    {
+        UIHelper.UIClose();
+    }
+    public void Exit()
+    {
+        gameObject.SetActive(false);
+    }
+    // Update is called once per frame
     void Update()
     {
         
     }
-    void ActiveButton()
-    {
-        foreach(int index in inventory.GetListNotes().Keys)
-        {
-            goListaDeNotas[index].gameObject.SetActive(true);
-        }        
-    }
-    public void OpenNotes()
-    {
-        if (notesAreOpen)
-        {
-            panelNotes.SetActive(false);
-            Cursor.lockState = CursorLockMode.Locked;
-            Cursor.visible = false;
-            cc.enabled = true;
-        }
-        else
-        {
-            panelNotes.SetActive(true);
-            ActiveButton();
-            Cursor.lockState = CursorLockMode.None;
-            Cursor.visible = true;
-            cc.enabled = false;
-        }
 
-        notesAreOpen = !notesAreOpen;
-    }
-    public void SetMouseForUIOpen()
+    public void Test()
     {
-        Cursor.lockState = CursorLockMode.None;
-        Cursor.visible = true;
-        cc.enabled = false;
-    }
-    public void SetMouseForUIClose()
-    {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-        cc.enabled = true;
+        
     }
 }
